d0d002a71ddc8cc810a26af728f7270b0d2e6a74
SemvandenBos <semvandenbos1@gmail.com>
2025-03-26 21:51:09 +0100

    message style

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,13 +4,12 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-    <!-- TODO, hoe kan dit mooi in een toast message -->
-    <div class="alert alert-info">
-        {{ session('status') }}
-    </div>
     
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-md shadow">
+                {{ session('status') }}
+            </div>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')

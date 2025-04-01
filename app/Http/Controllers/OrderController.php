ff0cccfae62e028863a69a3ad8b0a0726aa9a80d
SemvandenBos <semvandenbos1@gmail.com>
2025-04-01 18:45:07 +0200

    availability check in store method

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\RentalProduct;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,16 +14,21 @@ class OrderController extends Controller
         $validated = $request->validate([
             'product_id' => 'required|exists:rental_products,id',
         ]);
-    
+
         $user = Auth::user();
-    
+
+        $product = RentalProduct::findOrFail($validated['product_id']);
+        if (! $product->available()) {
+            return redirect()->back()->with('error', 'This product is currently not available for rent.');
+        }
+
         Order::create([
             'user_id' => $user->id,
             'rental_product_id' => $validated['product_id'],
             'rented_at' => now()->toDateString(),
             // 'return_due_at' => now()->addWeek()->toDateString(),
         ]);
-    
+
         return redirect()->back()->with('success', 'Product rented successfully!');
     }
 }

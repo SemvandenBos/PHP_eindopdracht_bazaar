4d2cf0685357ea532258c3f72ac06384deefdb6c
SemvandenBos <semvandenbos1@gmail.com>
2025-04-03 15:58:17 +0200

    favourite page navigable

--- resources/views/favourites.blade.php
+++ resources/views/favourites.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
     <x-card-list :items='$rentalFavourites'>
         @foreach ($rentalFavourites as $rentalFavourite)
-            <x-card :title='$rentalFavourite->name'>
-                <x-favourite-button :productId='$rentalFavourite->id' :isActive="Auth::user()->hasFavourite($rentalFavourite)"></x-favourite-button>
-            </x-card>
+            <a href="{{ route('rentalProduct.show', $rentalFavourite->id) }}">
+                <x-card :title="$rentalFavourite->name">
+                    <x-favourite-button :productId="$rentalFavourite->id" :isActive="Auth::user()->hasFavourite($rentalFavourite)"></x-favourite-button>
+                </x-card>
+            </a>
         @endforeach
     </x-card-list>
 </x-app-layout>

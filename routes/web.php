cbe399f0ad513fbdcd1d261d1ae6199ec33389d3
SemvandenBos <semvandenbos1@gmail.com>
2025-04-06 15:12:35 +0200

    web.php file cleanup

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('set-locale/{locale}', function ($locale) {
     if (in_array($locale, ['en', 'nl'])) {
         App::setLocale($locale);
@@ -30,66 +20,47 @@ Route::get('set-locale/{locale}', function ($locale) {
     return redirect()->back();
 })->name('locale.setting');
 
-Route::get('/register-advertiser', function () {
-    return view('register-advertiser.edit');
-})->middleware('auth')->name('register-advertiser');
-
-Route::get('/favourites', [FavouritesController::class, 'index'])->name('favourites');
-
 Route::patch('/profile.update-advertiser', [ProfileController::class, 'updateAdvertiser'])->middleware('auth')->name('profile.update-advertiser');
 
 Route::middleware(['auth'])->group(function () {
+    //Account
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::view('/register-advertiser', 'register-advertiser.edit')->name('register-advertiser');
+
     Route::get('/dashboard', function () {
         return view(view: 'dashboard');
     })->name('dashboard');
 
     Route::get('/personal-advertiser', function () {
         return "Personal Advertiser Dashboard";
-    })->middleware(RoleMiddleware::class . ':personal_advertiser')->name('personal-advertiser');
+    })->name('personal-advertiser');
 
     //Rental products
-    Route::resource('rentalProduct', RentalProductController::class)->names([
-        'index' => 'rentalProduct.index',
-        'create' => 'rentalProduct.create',
-        'store' => 'rentalProduct.store',
-        'show' => 'rentalProduct.show',
-    ]);
+    Route::resource('rentalProduct', RentalProductController::class)
+        ->only(['index', 'show'])
+        ->names([
+            'index' => 'rentalProduct.index',
+            'show' => 'rentalProduct.show',
+        ]);
     Route::get('rentedOverview', [RentalProductController::class, 'rentedOverview'])->name('rentalProduct/rentedOverview');
+    Route::get('/favourites', [FavouritesController::class, 'index'])->name('favourites');
 
     //Order
     Route::get('/order', [OrderController::class, 'index'])->name('order.index');
     Route::post('/order', [OrderController::class, 'store'])->name('order.store');
     Route::post('/orderReview', [OrderController::class, 'storeReview'])->name('order.storeReview');
     Route::post('/toggleFavourite', [OrderController::class, 'toggleFavourite'])->name('order.toggleFavourite');
+
+    //Advertisers routes:
+    Route::middleware(RoleMiddleware::class . ':personal_advertiser')->group(function () {
+        Route::get('rentalProduct.create', [RentalProductController::class, 'create'])->name('rentalProduct.create');
+        Route::post('rentalProduct.store', [RentalProductController::class, 'store'])->name('rentalProduct.store');
+        Route::post('rentalProduct.storeBulk', [RentalProductController::class, 'storeBulk'])->name('rentalProduct.storeBulk');
+        Route::get('rentalProduct.export', [RentalProductController::class, 'export'])->name('rentalProduct.export');
+    });
 });
-
-Route::middleware('auth', RoleMiddleware::class . ':personal_advertiser')->group(function () {
-    Route::get('rentalProduct.create', [RentalProductController::class, 'create'])->name('rentalProduct.create');
-    Route::post('rentalProduct.store', [RentalProductController::class, 'store'])->name('rentalProduct.store');
-    Route::post('rentalProduct.storeBulk', [RentalProductController::class, 'storeBulk'])->name('rentalProduct.storeBulk');
-    Route::get('rentalProduct.export', [RentalProductController::class, 'export'])->name('rentalProduct.export');
-});
-
-Route::middleware(['auth'])->group(function () {
-    // Route::get('/dashboard', function () {
-    //     return view(view: 'dashboard');
-    // })->name('dashboard');
-
-    // Route::get('/personal-advertiser', function () {
-    //     return "Personal Advertiser Dashboard";
-    // })->middleware(RoleMiddleware::class . ':personal_advertiser')->name('personal-advertiser');
-
-    Route::resource('rentalProduct', RentalProductController::class)->names([
-        'index' => 'rentalProduct.index',
-        'show' => 'rentalProduct.show',
-    ]);
-
-    Route::get('/order', [OrderController::class, 'index'])->name('order.index'); //TODO
-    Route::post('/order', [OrderController::class, 'store'])->name('order.store');
-    Route::post('/orderReview', [OrderController::class, 'storeReview'])->name('order.storeReview');
-    Route::post('/toggleFavourite', [OrderController::class, 'toggleFavourite'])->name('order.toggleFavourite');
-});
-
 
 
 require __DIR__ . '/auth.php';
